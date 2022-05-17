8297e137a454e795e2560bee2c687987aee5f9e6
ethan <ethan@monolabs.io>
2022-05-17 21:17:59 +0900

    Fix: fix

--- graph_3.py
+++ graph_3.py
@@ -6,14 +6,14 @@ import sys
 sys.setrecursionlimit(10 ** 4)  # 기본적으로 재귀 횟수를 1000 정도로 설정해놓음
 
 
-def dst(cur_pos: tuple):
+def dfs(cur_pos: tuple):
     visited[cur_pos[1]][cur_pos[0]] = True
     for _i in range(4):
         next_pos_x = cur_pos[0] + dx[_i]
         next_pos_y = cur_pos[1] + dy[_i]
         if 0 <= next_pos_x < m and 0 <= next_pos_y < n and position[next_pos_y][next_pos_x]:
             if not visited[next_pos_y][next_pos_x]:
-                dst((next_pos_x, next_pos_y))
+                dfs((next_pos_x, next_pos_y))
 
 
 dx = [-1, 1, 0, 0]
@@ -34,7 +34,7 @@ for idx in range(t):
     for i in range(n):
         for j in range(m):
             if position[i][j] and not visited[i][j]:
-                dst((j, i))
+                dfs((j, i))
                 cnt += 1
 
     rst.append(cnt)
