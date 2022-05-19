5cd123c7c0e370f834549890c18386d3ffb86d08
ethan <ethan@monolabs.io>
2022-05-19 23:50:27 +0900

    Add: 그리디 알고리즘 문제 구현

---
+++ greedy_9.py
@@ -0,0 +1,19 @@
+"""
+백준 - 등수 매기기
+"""
+
+import sys
+
+n = int(sys.stdin.readline())
+array = []
+
+for _ in range(n):
+    array.append(int(sys.stdin.readline()))
+
+array.sort()
+
+result = 0
+for i in range(1, len(array) + 1):
+    result += abs(i - array[i - 1])
+
+print(result)
